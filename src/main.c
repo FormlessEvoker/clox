42712c200c2b17b33cf7634a93ce33fb7dc36d9f
Formless Evoker <formless.evoker@proton.me>
2025-06-30 19:26:15 -0500

    basic VM

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
@@ -9,13 +11,51 @@ int main(int argc, const char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
 
-  int constant = addConstant(&chunk, 1.2);
+  int constant;
+
+  // // -((1.2 + 3.4) / 5.6)
+  constant = addConstant(&chunk, 1.2);
+
   writeChunk(&chunk, OP_CONSTANT, 123);
   writeChunk(&chunk, constant, 123);
 
+  constant = addConstant(&chunk, 3.4);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_ADD, 123);
+
+  constant = addConstant(&chunk, 5.6);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_DIVIDE, 123);
+  writeChunk(&chunk, OP_NEGATE, 123);
+
   writeChunk(&chunk, OP_RETURN, 123);
 
-  disassembleChunk(&chunk, "test chunk");
+  disassembleChunk(&chunk, "-((1.2 + 3.4) / 5.6)");
+
+  // 1 * 2 + 3
+  // constant = addConstant(&chunk, 1.0);
+  // writeChunk(&chunk, OP_CONSTANT, 123);
+  // writeChunk(&chunk, constant, 123);
+
+  // constant = addConstant(&chunk, 2.0);
+  // writeChunk(&chunk, OP_CONSTANT, 123);
+  // writeChunk(&chunk, constant, 123);
+
+  // writeChunk(&chunk, OP_MULTIPLY, 123);
+
+  // constant = addConstant(&chunk, 3.0);
+  // writeChunk(&chunk, OP_CONSTANT, 123);
+  // writeChunk(&chunk, constant, 123);
+
+  // writeChunk(&chunk, OP_ADD, 123);
+  // writeChunk(&chunk, OP_RETURN, 123);
+
+  // disassembleChunk(&chunk, "1 * 2 + 3");
+
   interpret(&chunk);
   freeVM();
   freeChunk(&chunk);
--- src/vm.c
+++ src/vm.c
@@ -11,9 +11,23 @@ static void resetStack() { vm.stackTop = vm.stack; }
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)                                                          \
+  do {                                                                         \
+    double b = pop();                                                          \
+    double a = pop();                                                          \
+    push(a op b);                                                              \
+  } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+    printf("STACK   | ");
+    for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[ ");
+      printValue(*slot);
+      printf(" ]");
+    }
+    printf("\n");
+
     disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif
 
@@ -21,17 +35,39 @@ static InterpretResult run() {
     switch (instruction = READ_BYTE()) {
     case OP_CONSTANT: {
       Value constant = READ_CONSTANT();
+      push(constant);
       printValue(constant);
       printf("\n");
       break;
     }
+    case OP_ADD:
+      BINARY_OP(+);
+      break;
+    case OP_SUBTRACT:
+      BINARY_OP(-);
+      break;
+    case OP_MULTIPLY:
+      BINARY_OP(*);
+      break;
+    case OP_DIVIDE:
+      BINARY_OP(/);
+      break;
+    case OP_NEGATE: {
+      // Negate the top value on the stack.
+      // Do this in place to prevent from popping and re-pushing to the stack.
+      *vm.stackTop *= -1;
+      break;
+    }
     case OP_RETURN:
+      printValue(pop());
+      printf("\n");
       return INTERPRET_OK;
     }
   }
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 void initVM() { resetStack(); }
